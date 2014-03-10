d06665f736e6a38925d537a5876d3e0bb10fed98
Drew <drew@drew-machine.(none)>
2014-03-09 23:53:29 -0700

    Cleaning up code style before modularizing.

--- js/jsonp.js
+++ js/jsonp.js
@@ -3,152 +3,225 @@
  *	@author Andrew Gatlabayan
  *
  */
-require(["jquery.jsonp-2.1.4.min.js","sprintf-0.7-beta1.js"],function(){
+require( [ 'jquery.jsonp-2.1.4.min.js' , 'sprintf-0.7-beta1.js' ], function ( ) {
 	var list;
 	var albumViewer;
 	var i = 0;
 	var albumCoverCollection = [];
-	
+
 
 	/**
 	 *  Album Cover Object Type
 	 *	@class
+     *	@param		{object} options
 	 *	@returns	{object|AlbumCover}
 	 */
-	function AlbumCover(o){
-		
-		/** Title of Album */
-		this.title;
-		
-		/** Source FIXME comment is incomplete */
-		this.source;
-		
-		/** Thumbnail image */
-		this.thumbnail;
-		
-		/** Fully qualified image url */
-		this.img;
-		
-		/** Reddit post id */
-		this.rId;
-		
-		/** Reddit post name */
-		this.rName;
-		
-		
+	function AlbumCover ( options ) {
+
 		// leave if we cannot populate the object
-        if (typeof o !== "object") {
-        	return;
+        if ( typeof options !== 'object' ) {
+            console.error( 'No data to create an album cover from' );
+        	return null;
         }
-        
-        this.title = o.title || null;
-        this.source = o.source || null;
-        this.thumbnail = o.thumbnail || null;
-        this.img = o.img || null;
-        this.rId = o.rId || null;
-        this.rName = o.rName || null;
+
+		/** Title of Album */
+        this.title = options.title || null;
+
+		/** Source FIXME comment is incomplete */
+        this.source = options.source || null;
+
+		/** Thumbnail image */
+        this.thumbnail = options.thumbnail || null;
+
+		/** Fully qualified image url */
+        this.img = options.img || null;
+
+		/** Reddit post id */
+        this.rId = options.rId || null;
+
+		/** Reddit post name */
+        this.rName = options.rName || null;
+
 	}
 
-	albumCoverCollection.saveJson = function(json){
+
+    albumCoverCollection.addAlbum = function ( options ) {
+
+        if ( !options ) {
+            console.error( 'Cannot add an empty album' );
+        }
+
+        return this.push( new AlbumCover( {
+            title: options.title,
+            source: 'http://www.reddit.com' + options.permalink,
+            thumbnail: options.thumbnail,
+            img: options.url,
+            rId: options.subreddit_id,
+            rName: options.name
+        } ) );
+
+    };
+
+
+	albumCoverCollection.saveJson = function ( json ) {
+
 		var results = json.data.children;
+
 		list = json;
-		for(var i = 0; i < results.length; i++){
-			(function(index){
-				var _this = results[index].data;
-				albumCoverCollection.push(new AlbumCover({
-					title: _this.title,
-					source: 'http://www.reddit.com' + _this.permalink,
-					thumbnail: _this.thumbnail,
-					img: _this.url,
-					rId: _this.subreddit_id,
-					rName: _this.name
-				}));
-			})(i);
+
+		for( var i = 0; i < results.length; i++ ) {
+            var albumData = results[index].data || null;
+
+            if ( !albumData ) {
+                continue;
+            }
+
+            albumCoverCollection.addAlbum( albumData );
+
 		}
+
+        // FIXME this should be moved to the caller of this function
 		albumViewer.updateListPane();
 	};
-	albumCoverCollection.getAlbum = function(i){
-		return this[i] || false;
+
+
+	albumCoverCollection.getAlbum = function ( index ) {
+		return this[ index ] || false;
 	};
-	albumCoverCollection.pullNext = function(callBack){
-		var lastAlbum = this.getAlbum(this.length - 1),
-		rName = (lastAlbum) ? '&after='+lastAlbum.rName : '';
+
+
+	albumCoverCollection.pullNext = function( callBack ) {
+
+		var lastAlbum = this.getAlbum( this.length - 1 );
+        // FIXME this code is confusing, refactor to clearly build the url string
+		var rName = ( lastAlbum ) ? '&after=' + lastAlbum.rName : '';
+
 		$.jsonp({
 			url: 'http://www.reddit.com/r/AlbumArtPorn/.json?limit=15'+rName+'&jsonp=albumCoverCollection.saveJson',
 			callbackParameter: '',
-			beforeSend: function(){
+			beforeSend: function () {
 				$("#albums h1:first").append(" | LOADING!");
 			},
-			complete: function(){
+			complete: function ( ) {
 				$("#albums h1:first").text($("#albums h1:first").text().replace(" | LOADING!",""));
 				if(typeof callBack === "function"){
 					callBack();
 				}
 			}
 		});
+
 	};
 
-	$(document).ready(function(){
-		albumViewer = (function(){
-			var _listPane = $(document.getElementById("results")).find("ul"),
-				_viewPane = $(document.getElementById("view")),
-				_viewImg = _viewPane.find("#view-img"),
-				_viewTitle = _viewPane.find("#view-title"),
-				_currentAlbum = 0;
-			
-			albumCoverCollection.pullNext();
+    // TODO move to another file
+	$(document).ready( function ( ) {
 
+		albumViewer = (function(){
+
+            // List View
+			var _listPane 		= $( '#results' ).find( 'ul');
+
+            // Main View
+            var _viewPane 		= $( '#view' );
+
+            // Main View's Image
+			var _viewImg 		= _viewPane.find( '#view-img' );
+
+            // Main View's Title
+			var _viewTitle 		= _viewPane.find( '#view-title' );
+
+            // Current Album ID
+			var _currentAlbum 	= 0;
+
+
+            // List View's Events
+            // ==================
 			_listPane
-				.bind("click.album",function(e){
+				.bind( 'click.album', function ( e ) {
+
 					var $this = $(e.target), album;
-					if($this.attr("acc-id")){
-						_viewPane.trigger("interactive.album");
-						album = albumCoverCollection.getAlbum($this.attr("acc-id"));
-						_viewImg.attr("src", album.img);
-						_viewTitle.text(album.title);
-						$this.parent().siblings(".result-active").removeClass("result-active").end().addClass("result-active");
-					}else if(e.target.tagName == "LI"){
-						$this.find("img").trigger("click.album");
+
+					if( $this.attr( 'acc-id' ) ) {
+						_viewPane.trigger( 'interactive.album ');
+
+						album = albumCoverCollection.getAlbum( $this.attr( 'acc-id' ) );
+
+						_viewImg.attr( 'src', album.img );
+
+						_viewTitle.text( album.title );
+
+						$this.parent().siblings( '.result-active' )
+                        	.removeClass( 'result-active' )
+                       		.end().addClass( 'result-active' );
+
+					} else if (e.target.tagName === 'LI') {
+
+						$this.find( 'img' ).trigger( 'click.album' );
+
 					}
-				})
-				.end().find("#results-list-anchor").bind("click.album",function(e){
+
+				} )
+				.end().find( '#results-list-anchor' ).bind( 'click.album' , function( e ) {
 					e.preventDefault();
 					albumCoverCollection.pullNext();
-				});
+				} );
 
-			_viewImg.bind("load.album",function(e){
-				_viewPane.trigger("ready.album");
+
+ 			// Main View Image's Events
+            // ==================
+			_viewImg.bind( 'load.album' , function( e ) {
+				_viewPane.trigger( 'ready.album' );
 			});
 
-			_viewPane
-				.bind("interactive.album",function(e){
-					$(this).children().hide().filter("#view-loading-spinner").show();
-				})
-				.bind("ready.album",function(e){
-					$(this).children().show().filter("#view-loading-spinner").hide()
-				})
-				.append('<span id="view-loading-spinner">Loading</span>');
 
+            // Main View's Events
+            // ==================
+			_viewPane
+				.bind( 'interactive.album' , function( e ) {
+					$(this)
+                    	.children().hide()
+                    	.filter( '#view-loading-spinner' ).show();
+				} )
+				.bind( 'ready.album', function ( e ) {
+					$(this)
+                    	.children().show()
+                   		.filter( '#view-loading-spinner' ).hide()
+				} )
+				.append( '<span id="view-loading-spinner">Loading</span>' );
+
+
+            // Init
+			albumCoverCollection.pullNext();
 			_viewImg.show();
 
 			return {
-				updateListPane: function(){
+				updateListPane: function ( ) {
+
 					for(i = _currentAlbum; i < albumCoverCollection.length; i++){
+                        // FIXME move logic to another function
 						(function(index){
+
 							_listPane.append(
-								sprintf('<li><img src="%(thumbnail)s" acc-id="'+index+'" alt="%(title)s"/>%(title)s</li>', albumCoverCollection.getAlbum(index))
+								sprintf(
+                                    '<li><img src="%(thumbnail)s" acc-id="'+index+'" alt="%(title)s"/>%(title)s</li>',
+                                    albumCoverCollection.getAlbum( index )
+                                )
 							);
+
 						})(i);
 					}
-					if(_currentAlbum === 0){
-						_listPane.find("img:first").trigger("click");
+
+					if ( _currentAlbum === 0 ) {
+						_listPane.find( 'img:first' ).trigger( 'click' );
 					}
+
+                    // FIXME this doesn't make sense, how the length _currentAlbum?
 					_currentAlbum = albumCoverCollection.length;
+
 				},
-				getCurrentId: function(){
+				getCurrentId: function ( ) {
 					return _currentAlbum;
 				},
-				changeView: function(url){
+				changeView: function( url ) {
 					_viewImg.attr("src", url);
 				}
 			};
